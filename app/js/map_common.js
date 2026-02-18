156bea30f8a5a133de3065db95c5dc877ee92bbb
Sven <svenkarmann@gmx.de>
2026-02-18 22:54:19 +0100

    fix movement stop on mobile

--- app/js/map_common.js
+++ app/js/map_common.js
@@ -272,7 +272,7 @@ function initMap(collectionName, styleFunction) {
   const map = new ol.Map({
     layers: [googleLayer],
     controls: ol.control.defaults.defaults({ attribution: false }).extend([attribution]),
-    interactions: ol.interaction.defaults.defaults({ kinetic: null }),
+    interactions: ol.interaction.defaults.defaults({ kinetic: new ol.Kinetic(-10, 0, 100) }),
     target: "map",
     view: new ol.View({
       center: ol.proj.fromLonLat([11.145, 48.765]),
